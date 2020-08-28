3f2fe974133ba50c61fb9d164b75d4e268c7175f
PariDot <dylan.rush@alttrasolutions.tech>
2020-08-28 11:57:35 -0600

    Initial commit with skeletal structure for the Cova poker test.

---
+++ PokerShowdown/PokerShowdown/PokerShowdown.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PokerShowdown
+{
+    public class PokerShowdown
+    {
+        /*
+        Purpose
+        Description
+         */
+        public static List<Player> GetWinner(List<Player> players)
+        {
+
+            return players;
+        }
+        
+
+    }
+
+    public class Player
+    {
+        String name;
+        List<Card> hand;
+
+        public Player(List<Card> _hand)
+        {
+            this.hand = _hand;
+        }
+        bool IsFlush()
+        {
+            return false;
+        }
+        bool IsThreeofKind()
+        {
+            return false;
+        }
+        bool IsOnePair()
+        {
+            return false;
+        }
+        Card IsHighCard()
+        {
+            return new Card();
+        }
+
+    }
+    public class Card
+    {
+        private int rank;
+        private Char suite;
+        public Card()
+        {
+            rank = -1;
+            suite = 'A';
+        }
+        Card(int _rank,char _suite)
+        {
+            this.suite = _suite;
+            this.rank = _rank;
+        }
+        public int GetRank()
+        {
+            return this.rank;
+        }
+        public char GetFace()
+        {
+            return this.suite;
+        }
+    }
+}
---
+++ PokerShowdownTesting/UnitTest1.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PokerShowdown;
+namespace PokerShowdownTesting
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+            
+        {
+
+            PokerShowdown.PokerShowdown.GetWinner();
+        }
+    }
+}
