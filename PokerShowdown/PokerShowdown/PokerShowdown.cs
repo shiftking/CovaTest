e038d2194a69b9706db165ccb2e7de7b043b92fd
PariDot <dylan.rush@alttrasolutions.tech>
2020-08-28 21:17:26 -0600

    Adding more fleshed out logic, also changed name of getter from face, to suite. added tests for helper functions and operator overloads for the hand. Also added the hand class to help abstract the comparison operators better.

--- PokerShowdown/PokerShowdown/PokerShowdown.cs
+++ PokerShowdown/PokerShowdown/PokerShowdown.cs
@@ -1,20 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
-
+/// <summary>
+/// Description Library for playing a simplified game of poker:
+///  Only plays of a subset of hands:
+///                         - Flush
+///                         - Three of a kind
+///                         - One Pair
+///                         - High card
+/// Assumptions:
+///     - Ace is always high, so rank 13
+///     - 
+/// </summary>
 namespace PokerShowdown
 {
+
+
     public class PokerShowdown
     {
-        /*
-        Purpose
-        Description
-         */
+        /// <summary>
+        ///  Purpose: Determines winners in a game of poker
+        ///  Description: Will return a list of players that are winning, from a list of players
+        /// 
+        /// </summary>
+
+
+         
         public static List<Player> GetWinner(List<Player> players)
         {
+            List<Player> winners = new List<Player>();
+            foreach(Player player in players)
+            {
+                //iterates through the players and determines the winners
+                // if the list doesn't have a winner, add the next one to the list
+                if(winners.Count == 0)
+                {
+                    winners.Add(player);
+                }
+                else
+                {
+                    foreach(Player winner in winners)
+                    {
+                        if(winner.GetHand() < player.GetHand())
+                        {
+                            winners = new List<Player>();
 
-            return players;
+                            winners.Add(player);
+                            //if the next player is a higher rank then we know that all other 
+                            //winners are not winners, so the winner list get cleared, and this new one gets added
+                            if (winner.GetHand() == player.GetHand())
+                            {
+                                winners.Add(player);
+                                //if the current player and the top winner are the same, than we add it to the list of winners
+                                return null;
+                            }
+                        }
+                    }
+                }
+                
+            }
+            return null;
         }
+
         
 
     }
@@ -22,40 +70,262 @@ namespace PokerShowdown
     public class Player
     {
         String name;
-        List<Card> hand;
+        private Hand hand;
+
 
         public Player(List<Card> _hand)
-        {
-            this.hand = _hand;
+        {   
+
+            if(_hand.Count != 5)
+            {
+                throw new Exception("Not the correct number of cards");
+            }
+            this.hand = new Hand(_hand);
         }
-        bool IsFlush()
+        public  Hand GetHand()
         {
+            return hand;
+        }
+        
+    }
+    public class Hand
+    {
+        private List<Card> cards;
+        
+        public Hand(List<Card> _cards)
+        {
+            this.cards = _cards;
+            this.cards.Sort();
+        }
+        public bool IsFlush()
+        {
+            //get the suite of the first card
+            char handSuite = this.cards.ToArray()[0].GetSuite();
+
+            foreach (Card card in this.cards)
+            {
+                if (card.GetSuite() != handSuite)
+                {
+                    return false;
+                }
+
+            }
+            return true;
+        }
+        public bool IsThreeofKind()
+        {
+            foreach (Card card in this.cards)
+            {
+                List<Card> likeCards = this.cards.FindAll(delegate (Card c) { return c.GetRank() == card.GetRank(); });
+                if(likeCards.Count == 3)
+                {
+                    
+                    return true;
+                }
+            }
             return false;
         }
-        bool IsThreeofKind()
+        public bool IsOnePair()
         {
+
+            foreach (Card card in this.cards)
+            {
+                List<Card> likeCards = this.cards.FindAll(delegate (Card c) { return c.GetRank() == card.GetRank(); });
+                if (likeCards.Count == 2)
+                {
+                    
+                    return true;
+                }
+            }
             return false;
         }
-        bool IsOnePair()
+        public Card HighCard()
         {
-            return false;
+            return this.cards.ToArray()[0];
         }
-        Card IsHighCard()
+        public Card GetDouplicate()
         {
-            return new Card();
+            foreach (Card card in this.cards)
+            {
+                List<Card> likeCards = this.cards.FindAll(delegate (Card c) { return c.GetRank() == card.GetRank(); });
+                if (likeCards.Count >= 2)
+                {
+
+                    return card;
+                }
+            }
+            return new Card(-1,' ');
+        }
+        public Card getCard(int i)
+        {
+            if (i < 0 || i >= 5)
+            {
+                throw new IndexOutOfRangeException("Can only access 0-4 index for hands");
+            }
+            return this.cards.ToArray()[i];
+        }
+        public static bool operator == (Hand a, Hand b)
+        {
+            if (a.IsFlush() && b.IsFlush())
+            {
+                foreach(Card cardA in a.cards)
+                {
+                    foreach(Card cardB in b.cards)
+                    {
+                        if (cardA != cardB)
+                        {
+                            return false;
+                        }
+                    }
+                    
+                }
+                return true;
+            }
+            if (a.IsThreeofKind() && b.IsThreeofKind())
+            {
+                if (a.GetDouplicate() > b.GetDouplicate())
+                {
+                    return true;
+                }
+                return true;
+            }
+            if (a.IsOnePair() && b.IsOnePair())
+            {
+                if (a.GetDouplicate() > b.GetDouplicate())
+                {
+                    return true;
+                }
+
+            }
+
+            return true;
+        }
+        public override bool Equals(object o)
+        {
+            if(o == null)
+            {
+                return false;
+            }
+            var b = o as Hand;
+            if (this.IsFlush() && b.IsFlush())
+            {
+                foreach (Card cardA in this.cards)
+                {
+                    foreach (Card cardB in b.cards)
+                    {
+                        if (cardA != cardB)
+                        {
+                            return false;
+                        }
+                    }
+
+                }
+                return true;
+            }
+            if (this.IsThreeofKind() && b.IsThreeofKind())
+            {
+                if (this.GetDouplicate() > b.GetDouplicate())
+                {
+                    return true;
+                }
+                return true;
+            }
+            if (this.IsOnePair() && b.IsOnePair())
+            {
+                if (this.GetDouplicate() > b.GetDouplicate())
+                {
+                    return true;
+                }
+
+            }
+
+            return true;
+        }
+        public override int GetHashCode()
+        {
+            return this.cards.Count;
+        }
+        public static bool operator < (Hand a, Hand b)
+        {
+
+            if (a.IsFlush() && b.IsFlush())
+            {
+                if (a.HighCard() < b.HighCard())
+                {
+                    return true;
+                }
+                return true;
+            }
+            if (a.IsThreeofKind() && b.IsThreeofKind())
+            {
+                if (a.GetDouplicate() < b.GetDouplicate())
+                {
+                    return true;
+                }
+                return true;
+            }
+            if (a.IsOnePair() && b.IsOnePair())
+            {
+                if (a.GetDouplicate() < b.GetDouplicate())
+                {
+                    return true;
+                }
+
+            }
+            return true;
+        }
+        public static bool operator > (Hand a, Hand b)
+        {
+            if(a.IsFlush() && b.IsFlush())
+            {
+                if(a.HighCard() > b.HighCard())
+                {
+                    return true;
+                }
+                return true;
+            }
+            if(a.IsThreeofKind() && b.IsThreeofKind())
+            {
+                if (a.GetDouplicate() > b.GetDouplicate())
+                {
+                    return true;
+                }
+                return true;
+            }
+            if(a.IsOnePair() && b.IsOnePair())
+            {
+                if (a.GetDouplicate() > b.GetDouplicate())
+                {
+                    return true;
+                }
+
+            }
+            return true;
+        }
+        public static bool operator !=(Hand a, Hand b)
+        {
+            return true;
         }
 
     }
-    public class Card
+   /// <summary>
+   /// Purpose: Represents a card in a poker game
+   /// Description: Holds the information that represents a valid card
+   /// [1 - 13] [Clubs(C), Hearts(H), Spades(S), Diamonds(D)] 
+   /// </summary>
+    public class Card : IComparable
     {
         private int rank;
         private Char suite;
+        
         public Card()
         {
             rank = -1;
             suite = 'A';
+
         }
-        Card(int _rank,char _suite)
+
+        public Card(int _rank, char _suite)
         {
             this.suite = _suite;
             this.rank = _rank;
@@ -64,9 +334,39 @@ namespace PokerShowdown
         {
             return this.rank;
         }
-        public char GetFace()
+        public char GetSuite()
         {
             return this.suite;
         }
+
+        public int  CompareTo(object other)
+        {
+            if(other == null)
+            {
+                return 1;
+            }
+            var card = other as Card;
+                           
+            return card.rank.CompareTo(this.rank);
+            
+        }
+
+        public static bool operator == (Card a, Card b)
+        {
+            return a.rank == b.rank;
+        }
+        public static bool operator !=(Card a, Card b)
+        {
+            return a.rank != b.rank;
+        }
+        public static bool operator >(Card a, Card b)
+        {
+            return a.rank > b.rank;
+        }
+        public static bool operator <(Card a, Card b)
+        {
+            return a.rank < b.rank;
+        }
     }
+        
 }
--- PokerShowdownTesting/UnitTest1.cs
+++ PokerShowdownTesting/UnitTest1.cs
@@ -1,16 +1,222 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PokerShowdown;
+
+using System.IO;
+using System;
+using System.Collections.Generic;
+
 namespace PokerShowdownTesting
 {
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
-            
+        public void TestIsFlsuh()
+
+        {
+            List<PokerShowdown.Card> hand = new List<Card>();
+            hand.Add(new Card(13, 'D'));
+            hand.Add(new Card(12, 'D'));
+            hand.Add(new Card(11, 'D'));
+            hand.Add(new Card(10, 'D'));
+            hand.Add(new Card(9, 'D'));
+            PokerShowdown.Player player = new Player(hand);
+
+            Assert.AreEqual(true, player.GetHand().IsFlush());
+
+            hand = new List<Card>();
+            hand.Add(new Card(13, 'D'));
+            hand.Add(new Card(12, 'D'));
+            hand.Add(new Card(11, 'S'));
+            hand.Add(new Card(10, 'D'));
+            hand.Add(new Card(9, 'D'));
+            player = new Player(hand);
+            Assert.AreEqual(false, player.GetHand().IsFlush());
+        }
+        [TestMethod]
+        ///Test the three of kind check
+        public void TestIsThreeOfKind()
+
         {
 
-            PokerShowdown.PokerShowdown.GetWinner();
+            List<PokerShowdown.Card> hand = new List<Card>();
+            hand.Add(new Card(13, 'D'));
+            hand.Add(new Card(13, 'S'));
+            hand.Add(new Card(13, 'C'));
+            hand.Add(new Card(10, 'D'));
+            hand.Add(new Card(9, 'D'));
+            PokerShowdown.Player player = new Player(hand);
+
+            Assert.AreEqual(true, player.GetHand().IsThreeofKind());
+
+            hand = new List<Card>();
+            hand.Add(new Card(13, 'D'));
+            hand.Add(new Card(13, 'S'));
+            hand.Add(new Card(12, 'C'));
+            hand.Add(new Card(10, 'D'));
+            hand.Add(new Card(9, 'D'));
+            player = new Player(hand);
+
+            Assert.AreEqual(false, player.GetHand().IsThreeofKind());
+            hand = new List<Card>();
+            hand.Add(new Card(13, 'D'));
+            hand.Add(new Card(13, 'S'));
+            hand.Add(new Card(12, 'C'));
+            hand.Add(new Card(13, 'C'));
+            hand.Add(new Card(9, 'D'));
+            player = new Player(hand);
+
+            Assert.AreEqual(true, player.GetHand().IsThreeofKind());
         }
+        [TestMethod]
+        public void TestIsOnePair()
+
+        {
+
+            List<PokerShowdown.Card> hand = new List<Card>();
+            hand.Add(new Card(13, 'D'));
+            hand.Add(new Card(13, 'S'));
+            hand.Add(new Card(10, 'C'));
+            hand.Add(new Card(1, 'D'));
+            hand.Add(new Card(9, 'D'));
+            PokerShowdown.Player player = new Player(hand);
+
+            Assert.AreEqual(true, player.GetHand().IsOnePair());
+
+            hand = new List<Card>();
+            hand.Add(new Card(13, 'D'));
+            hand.Add(new Card(11, 'S'));
+            hand.Add(new Card(12, 'C'));
+            hand.Add(new Card(10, 'D'));
+            hand.Add(new Card(9, 'D'));
+            player = new Player(hand);
+
+            Assert.AreEqual(false, player.GetHand().IsOnePair());
+            hand = new List<Card>();
+            hand.Add(new Card(12, 'D'));
+            hand.Add(new Card(10, 'S'));
+            hand.Add(new Card(12, 'C'));
+            hand.Add(new Card(13, 'C'));
+            hand.Add(new Card(9, 'D'));
+            player = new Player(hand);
+
+            Assert.AreEqual(true, player.GetHand().IsOnePair());
+        }
+        [TestMethod]
+        public void TestHighCard()
+
+        {
+
+            List<PokerShowdown.Card> hand = new List<Card>();
+            hand.Add(new Card(13, 'D'));
+            hand.Add(new Card(13, 'S'));
+            hand.Add(new Card(10, 'C'));
+            hand.Add(new Card(1, 'D'));
+            hand.Add(new Card(9, 'D'));
+            PokerShowdown.Player player = new Player(hand);
+
+            Assert.AreEqual(13, player.GetHand().HighCard().GetRank());
+
+            hand = new List<Card>();
+            hand.Add(new Card(10, 'D'));
+            hand.Add(new Card(11, 'S'));
+            hand.Add(new Card(12, 'C'));
+            hand.Add(new Card(10, 'D'));
+            hand.Add(new Card(9, 'D'));
+            player = new Player(hand);
+
+            Assert.AreEqual(12, player.GetHand().HighCard().GetRank());
+
+            hand = new List<Card>();
+            hand.Add(new Card(4, 'D'));
+            hand.Add(new Card(1, 'S'));
+            hand.Add(new Card(1, 'C'));
+            hand.Add(new Card(4, 'C'));
+            hand.Add(new Card(9, 'D'));
+            player = new Player(hand);
+
+            Assert.AreEqual(9, player.GetHand().HighCard().GetRank());
+        }
+
+        [TestMethod]
+        public void TestGetCard()
+
+        {
+
+            List<PokerShowdown.Card> hand = new List<Card>();
+            hand.Add(new Card(13, 'D'));
+            hand.Add(new Card(13, 'S'));
+            hand.Add(new Card(10, 'C'));
+            hand.Add(new Card(1, 'D'));
+            hand.Add(new Card(9, 'D'));
+            PokerShowdown.Player player = new Player(hand);
+            //should pull the highest card
+            Assert.AreEqual(13, player.GetHand().getCard(0).GetRank());
+
+            hand = new List<Card>();
+            hand.Add(new Card(10, 'D'));
+            hand.Add(new Card(11, 'S'));
+            hand.Add(new Card(12, 'C'));
+            hand.Add(new Card(10, 'D'));
+            hand.Add(new Card(9, 'D'));
+            player = new Player(hand);
+            //third highest card
+            Assert.AreEqual(10, player.GetHand().getCard(2).GetRank());
+
+            hand = new List<Card>();
+            hand.Add(new Card(4, 'D'));
+            hand.Add(new Card(1, 'S'));
+            hand.Add(new Card(1, 'C'));
+            hand.Add(new Card(4, 'C'));
+            hand.Add(new Card(9, 'D'));
+            player = new Player(hand);
+            //getting 2nd highest card
+            Assert.AreEqual(4, player.GetHand().getCard(1).GetRank());
+        }
+        [TestMethod]
+        public void TestGetDouplicate()
+
+        {
+
+            List<PokerShowdown.Card> hand = new List<Card>();
+            hand.Add(new Card(13, 'D'));
+            hand.Add(new Card(13, 'S'));
+            hand.Add(new Card(10, 'C'));
+            hand.Add(new Card(1, 'D'));
+            hand.Add(new Card(9, 'D'));
+            PokerShowdown.Player player = new Player(hand);
+            //should pull the highest card
+            Assert.AreEqual(13, player.GetHand().GetDouplicate().GetRank());
+
+            hand = new List<Card>();
+            hand.Add(new Card(10, 'D'));
+            hand.Add(new Card(11, 'S'));
+            hand.Add(new Card(12, 'C'));
+            hand.Add(new Card(10, 'D'));
+            hand.Add(new Card(9, 'D'));
+            player = new Player(hand);
+            //third highest card
+            Assert.AreEqual(10, player.GetHand().GetDouplicate().GetRank());
+
+            hand = new List<Card>();
+            hand.Add(new Card(4, 'D'));
+            hand.Add(new Card(1, 'S'));
+            hand.Add(new Card(1, 'C'));
+            hand.Add(new Card(4, 'C'));
+            hand.Add(new Card(9, 'D'));
+            player = new Player(hand);
+            //getting 2nd highest card
+            Assert.AreEqual(4, player.GetHand().GetDouplicate().GetRank());
+            hand = new List<Card>();
+            hand.Add(new Card(4, 'D'));
+            hand.Add(new Card(1, 'S'));
+            hand.Add(new Card(2, 'C'));
+            hand.Add(new Card(3, 'C'));
+            hand.Add(new Card(9, 'D'));
+            player = new Player(hand);
+            //getting 2nd highest card
+            Assert.AreEqual(-1, player.GetHand().GetDouplicate().GetRank());
+        }
+
     }
 }
